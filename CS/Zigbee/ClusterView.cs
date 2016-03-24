b9914294758230774b195ab184c39f4132021bc0
Quan Chen <chenq182@sina.com>
2016-03-24 12:29:40 +0800

    Added files via upload

---
+++ CS/Zigbee/ClusterView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Permissions;
+
+namespace Zigbee
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class ClusterView : Form
+    {
+        public ClusterView(string str)
+        {
+            InitializeComponent();
+            Uri url = new Uri(str);
+            webBrowser.Url = url;
+            webBrowser.ObjectForScripting = this;
+            this.ResumeLayout(false);
+        }
+        public string setIP(string ip)
+        {
+            MainForm.GuideClient.Server = ip;
+            MainForm.WriteClient.Server = ip;
+            MainForm.ReadClient.Server = ip;
+            if (MainForm.GuideClient.Active())
+            {
+                return "Succeed!";
+            }
+            else
+            {
+                return "Failed!";
+            }
+        }
+    }
+}
