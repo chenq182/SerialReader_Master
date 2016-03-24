24c1bdb6b45e14f0e5b751d89719119a531f9e28
Quan Chen <chenq182@sina.com>
2016-03-24 12:07:51 +0800

    Create LocateMap.Designer.cs

---
+++ CS/Zigbee/Map/LocateMap.Designer.cs
@@ -0,0 +1,58 @@
+namespace Zigbee
+{
+    partial class LocateMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(500, 400);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // LocateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "LocateForm";
+            this.ShowIcon = false;
+            this.Text = "节点坐标设置";
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
