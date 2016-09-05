b0285e0701a8194260c9d6533667c7b9471ec097
Quan Chen <chenq182@sina.com>
2016-09-05 14:59:31 +0800

    Update setup.sh

--- v3/setup.sh
+++ v3/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get install mysql-server
-sudo apt-get install python-pip
+#sudo apt-get install python-pip
+#sudo apt-get remove python-pip
+sudo easy_install pip
 sudo apt-get install python2.7-dev
 sudo apt-get install libmysqlclient-dev
 sudo pip install mysql-python
